d73e804c395699d372dd16e4df22cf83ceed8cae
Ryan <ryan.c.edge@outlook.com>
2020-05-21 22:08:54 -0400

    feat: add tests for rx & get_state

---
+++ test/get_rxstate_test.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+void main() {
+  testWidgets("GetController smoke test", (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: GetX<Controller>(
+          init: Controller(),
+          builder: (controller) {
+            return Column(
+              children: [
+                Text(
+                  'Count: ${controller.counter.value}',
+                ),
+                Text(
+                  'Double: ${controller.doubleNum.value}',
+                ),
+                Text(
+                  'String: ${controller.string.value}',
+                ),
+                Text(
+                  'List: ${controller.list.length}',
+                ),
+                Text(
+                  'Bool: ${controller.boolean.value}',
+                ),
+                Text(
+                  'Map: ${controller.map.value.length}',
+                ),
+                FlatButton(
+                  child: Text("increment"),
+                  onPressed: () => controller.increment(),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
+
+    expect(find.text("Count: 0"), findsOneWidget);
+    expect(find.text("Double: 0.0"), findsOneWidget);
+    expect(find.text("String: string"), findsOneWidget);
+    expect(find.text("Bool: true"), findsOneWidget);
+    expect(find.text("List: 0"), findsOneWidget);
+    expect(find.text("Map: 0"), findsOneWidget);
+
+    Controller.to.increment();
+
+    await tester.pump();
+
+    expect(find.text("Count: 1"), findsOneWidget);
+
+    await tester.tap(find.text('increment'));
+
+    await tester.pump();
+
+    expect(find.text("Count: 2"), findsOneWidget);
+  });
+}
+
+class Controller extends RxController {
+  static Controller get to => Get.find();
+
+  var counter = 0.obs;
+  var doubleNum = 0.0.obs;
+  var string = "string".obs;
+  var list = [].obs;
+  var map = {}.obs;
+  var boolean = true.obs;
+
+  void increment() {
+    counter.value++;
+  }
+}
