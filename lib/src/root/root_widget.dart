185ae9ba48067d2e0d7930e3d06e347a426b28aa
Jonny Borges <35742643+jonataslaw@users.noreply.github.com>
2020-07-18 13:28:42 -0300

    Added comments to language extensions

--- lib/src/root/root_widget.dart
+++ lib/src/root/root_widget.dart
@@ -257,16 +257,22 @@ abstract class Translations {
 
 extension Trans on String {
   String get tr {
+    // Returns the key if locale is null.
     if (Get.locale?.languageCode == null) return this;
+    
+    // Checks whether the language code and country code are present, and whether the key is also present.
     if (Get.translations.containsKey(
             "${Get.locale.languageCode}_${Get.locale.countryCode}") &&
         Get.translations["${Get.locale.languageCode}_${Get.locale.countryCode}"]
             .containsKey(this)) {
       return Get.translations[
           "${Get.locale.languageCode}_${Get.locale.countryCode}"][this];
+    
+    // Checks if there is a callback language in the absence of the specific country, and if it contains that key.  
     } else if (Get.translations.containsKey(Get.locale.languageCode) &&
         Get.translations[Get.locale.languageCode].containsKey(this)) {
       return Get.translations[Get.locale.languageCode][this];
+    // If there is no corresponding language or corresponding key, return the key.
     } else {
       return this;
     }
