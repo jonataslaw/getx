8988d7ee17942bcf2d2b40aa7aec4fb307bc8add
Rodrigo Lopez Peker <roipeker@gmail.com>
2020-08-25 11:40:15 -0300

    - comment Obx functionality. - close subscription in Obx - added the experimental ObxValue() widget to manage local states based on Rx. - added experimental ValueBuilder(), analogue to ObxValue(), but uses a simpler StatefulWidget with a callback.

--- lib/src/state_manager/simple/simple_builder.dart
+++ lib/src/state_manager/simple/simple_builder.dart
@@ -1,6 +1,77 @@
+import 'dart:async';
 import 'dart:collection';
+
 import 'package:flutter/widgets.dart';
+
 import 'get_state.dart';
+
+typedef ValueBuilderUpdateCallback<T> = void Function(T snapshot);
+typedef ValueBuilderBuilder<T> = Widget Function(
+    T snapshot, ValueBuilderUpdateCallback<T> updater);
+
+/// Manages a local state like ObxValue, but uses a callback instead of a Rx value.
+///
+/// Sample:
+///  ValueBuilder<bool>(
+///    initialValue: false,
+///    builder: (value, update) => Switch(
+///    value: value,
+///    onChanged: (flag) {
+///       update( flag );
+///    },),
+///    onUpdate: (value) => print("Value updated: $value"),
+///  ),
+class ValueBuilder<T> extends StatefulWidget {
+  final T initialValue;
+  final ValueBuilderBuilder builder;
+  final void Function() onDispose;
+  final void Function(T) onUpdate;
+
+  const ValueBuilder({
+    Key key,
+    this.initialValue,
+    this.onDispose,
+    this.onUpdate,
+    @required this.builder,
+  }) : super(key: key);
+
+  @override
+  _ValueBuilderState<T> createState() => _ValueBuilderState<T>();
+}
+
+class _ValueBuilderState<T> extends State<ValueBuilder<T>> {
+  T value;
+
+  @override
+  void initState() {
+    super.initState();
+    value = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(value, updater);
+
+  void updater(T newValue) {
+    if (widget.onUpdate != null) {
+      widget.onUpdate(newValue);
+    }
+    setState(() {
+      value = newValue;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget?.onDispose?.call();
+    if (value is ChangeNotifier) {
+      (value as ChangeNotifier)?.dispose();
+    } else if (value is StreamController) {
+      (value as StreamController)?.close();
+    }
+    value = null;
+  }
+}
 
 // It's a experimental feature
 class SimpleBuilder extends StatefulWidget {
