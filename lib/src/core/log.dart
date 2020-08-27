444a27399b76483c091fbce636281086b181e6f1
Jorge Gatica <44851465+jorgegaticav@users.noreply.github.com>
2020-08-27 22:46:36 +0200

    Update log.dart

--- lib/src/core/log.dart
+++ lib/src/core/log.dart
@@ -5,5 +5,5 @@ import 'dart:developer' as developer;
 typedef LogWriterCallback = void Function(String text, {bool isError});
 
 void defaultLogWriterCallback(String value, {bool isError = false}) {
-  if (isError || GetConfig.isLogEnable) developer.log(value, name: '[GETX]');
+  if (isError || GetConfig.isLogEnable) developer.log(value, name: 'GETX');
 }
