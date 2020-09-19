9c7cbd0e493666df15745967af0ed4da0992240f
Jonatas <jonataslaw@gmail.com>
2020-09-19 09:52:06 -0300

    clean exports

--- getx/lib/get.dart
+++ getx/lib/get.dart
@@ -2,7 +2,8 @@ library get;
 
 export 'package:get_core/get_core.dart';
 export 'package:get_test/get_test.dart';
-export 'instance_manager.dart';
-export 'route_manager.dart';
-export 'state_manager.dart';
-export 'utils.dart';
+export 'package:get_state_manager/get_state_manager.dart';
+export 'package:get_instance/get_instance.dart';
+export 'package:get_navigation/get_navigation.dart';
+export 'package:get_utils/get_utils.dart';
+export 'package:get_rx/get_rx.dart';
