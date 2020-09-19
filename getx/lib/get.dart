2fa46c3a635c7f666e1f65b7b3ed3a5dba0c9381
Jonatas <jonataslaw@gmail.com>
2020-09-19 10:40:10 -0300

    update files

--- getx/lib/get.dart
+++ getx/lib/get.dart
@@ -6,4 +6,5 @@ export 'package:get_state_manager/get_state_manager.dart';
 export 'package:get_instance/get_instance.dart';
 export 'package:get_navigation/get_navigation.dart';
 export 'package:get_utils/get_utils.dart';
+
 export 'package:get_rx/get_rx.dart';
