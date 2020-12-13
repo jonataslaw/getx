fa2cf43f2a3697ee7c03c2c898fcf4256901c38c
Eduardo Florence <eduardo@geminisistemas.com>
2020-12-12 23:38:25 -0300

    Declared the missing methods in BaseWebSocket (stub)

--- lib/get_connect/sockets/src/sockets_stub.dart
+++ lib/get_connect/sockets/src/sockets_stub.dart
@@ -1,3 +1,5 @@
+import './socket_notifier.dart';
+
 class BaseWebSocket {
   String url;
   Duration ping;
@@ -10,7 +12,44 @@ class BaseWebSocket {
     throw 'To use sockets you need dart:io or dart:html';
   }
 
+  Future connect() async {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
+  void onOpen(OpenSocket fn) {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
+  void onClose(CloseSocket fn) {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
+  void onError(CloseSocket fn) {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
+  void onMessage(MessageSocket fn) {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+  
+  void on(String event, MessageSocket message) {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
   void close([int status, String reason]) {
     throw 'To use sockets you need dart:io or dart:html';
   }
+
+  void send(dynamic data) async {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
+  void dispose() {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
+  void emit(String event, dynamic data) {
+    throw 'To use sockets you need dart:io or dart:html';
+  }
+
 }
