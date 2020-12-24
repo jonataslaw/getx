02a952ec6f51733fed34df8c986a86e3d1ed0501
Jonatas <jonataslaw@gmail.com>
2020-12-24 20:12:53 -0300

    add supercontroller and improve upload time

--- example/lib/pages/home/presentation/controllers/home_controller.dart
+++ example/lib/pages/home/presentation/controllers/home_controller.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import '../../domain/adapters/repository_adapter.dart';
 import '../../domain/entity/cases_model.dart';
 
-class HomeController extends GetxController with StateMixin<CasesModel> {
+class HomeController extends SuperController<CasesModel> {
   HomeController({this.homeRepository});
 
   /// inject repo abstraction dependency
@@ -20,5 +20,62 @@ class HomeController extends GetxController with StateMixin<CasesModel> {
     }, onError: (err) {
       change(null, status: RxStatus.error(err.toString()));
     });
+  }
+
+  @override
+  void onReady() {
+    print('The build method is done. '
+        'Your controller is ready to call dialogs and snackbars');
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    print('onClose called');
+    super.onClose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    print('the window size did change');
+    super.didChangeMetrics();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    print('platform change ThemeMode');
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  Future<bool> didPushRoute(String route) {
+    print('the route $route will be open');
+    return super.didPushRoute(route);
+  }
+
+  @override
+  Future<bool> didPopRoute() {
+    print('the current route will be closed');
+    return super.didPopRoute();
+  }
+
+  @override
+  void onDetached() {
+    print('onDetached called');
+  }
+
+  @override
+  void onInactive() {
+    print('onInative called');
+  }
+
+  @override
+  void onPaused() {
+    print('onPaused called');
+  }
+
+  @override
+  void onResumed() {
+    print('onResumed called');
   }
 }
