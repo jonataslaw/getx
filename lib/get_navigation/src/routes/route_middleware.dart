a8c4f21c5b35fd2a89677a99480c2108eb35b05a
Eduardo Florence <eduardo@geminisistemas.com>
2020-12-29 21:32:49 -0300

    Add Page Parameter

--- lib/get_navigation/src/routes/route_middleware.dart
+++ lib/get_navigation/src/routes/route_middleware.dart
@@ -206,6 +206,7 @@ class PageRedirect {
   /// check if redirect is needed
   bool needRecheck() {
     final match = Get.routeTree.matchRoute(settings.name);
+    Get.parameters = match?.parameters;
 
     // No Match found
     if (match?.route == null) {
@@ -215,7 +216,7 @@ class PageRedirect {
 
     final runner = MiddlewareRunner(match.route.middlewares);
     route = runner.runOnPageCalled(match.route);
-    Get.parameters = route?.parameter;
+    addPageParameter(route);
 
     // No middlewares found return match.
     if (match.route.middlewares == null || match.route.middlewares.isEmpty) {
@@ -227,5 +228,17 @@ class PageRedirect {
     }
     settings = newSettings;
     return true;
+  }
+
+  void addPageParameter(GetPage route) {
+    if (route.parameter == null) return;
+
+    if (Get.parameters == null) {
+      Get.parameters = route.parameter;
+    } else {
+      final parameters = Get.parameters;
+      parameters.addEntries(route.parameter.entries);
+      Get.parameters = parameters;
+    }
   }
 }
