5b058b1c7682b58736463464ee484d87ffbd892a
Jonny Borges <35742643+jonataslaw@users.noreply.github.com>
2021-11-29 12:20:38 -0300

    fix internationalization and add tests to tr

--- test/navigation/utils/wrapper.dart
+++ test/navigation/utils/wrapper.dart
@@ -20,6 +20,8 @@ class Wrapper extends StatelessWidget {
     return GetMaterialApp(
       defaultTransition: defaultTransition,
       initialRoute: initialRoute,
+      translations: WrapperTranslations(),
+      locale: WrapperTranslations.locale,
       getPages: namedRoutes,
       home: Scaffold(
         body: child,
@@ -50,4 +52,22 @@ class WrapperNamed extends StatelessWidget {
       getPages: namedRoutes,
     );
   }
+}
+
+class WrapperTranslations extends Translations {
+  static final fallbackLocale = Locale('en', 'US');
+  static Locale? get locale => Locale('en', 'US');
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {
+          'covid': 'Corona Virus',
+          'total_confirmed': 'Total Confirmed',
+          'total_deaths': 'Total Deaths',
+        },
+        'pt_BR': {
+          'covid': 'Corona Vírus',
+          'total_confirmed': 'Total confirmado',
+          'total_deaths': 'Total de mortes',
+        },
+      };
 }
