ec6b37b8f98021443ad1646f2d58c4eda1ee70a7
SeaRoth <coryr32@gmail.com>
2022-02-21 11:21:36 -0800

    Update dashboard_controller.dart

--- example_nav2/lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ example_nav2/lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -62,24 +62,17 @@ class DashboardController extends OurController {
       getChallengerPlayers();
     });
 
-    // await Future.delayed(const Duration(seconds: 2), () async {
-    //   final s = await getSummoner(false, "Doublelift", fallbackAPI: true);
-    //   print(s);
-    //   if (s != null) {
-    //     final histories = await getMatchHistories(false, s.puuid??"", fallbackAPI: true);
-    //     var list = <Match>[];
-    //     final ten = histories?.take(10);
-    //     await Future.forEach(ten!, (String element) async {
-    //       final m = await getMatch(element);
-    //       print("got match ${m?.match?.info?.gameId}");
-    //       list.add(m?.match??Match());
-    //       Future.delayed(Duration(milliseconds: 500));
-    //     });
-    //     print("should have ${list.length} matches");
-    //     final that = await LeagueHelper().findMostPlayedChampions(s.puuid??"", list);
-    //     print("champion played: ${that.entries.first.key}");
-    //   }
+    // await Future.delayed(const Duration(seconds: 1), () async {
+    //     final dl = await league.getSummonerFromAPI("doublelift");
+    //
+    //     print(dl.summoner?.name);
+    //
+    //     final matchH = await league.getMatchesFromAPI(dl.summoner?.puuid??"");
+    //
+    //     print("${matchH.matchOverviews}");
+    //
     // });
+    
     super.onReady();
   }
 
