9f93f8eb2a2deef10ed01bde1f6be7c12f3fba5f
Nikita Aksakov <artcodefun@gmail.com>
2022-05-31 18:53:29 +0300

    fixed : GetBuilder & GetX not closing controllers when global = false

--- lib/get_state_manager/src/simple/get_state.dart
+++ lib/get_state_manager/src/simple/get_state.dart
@@ -480,9 +480,13 @@ class BindElement<T> extends InheritedElement {
 
   void dispose() {
     widget.dispose?.call(this);
-    if (_isCreator! || widget.assignId) {
-      if (widget.autoRemove && GetInstance().isRegistered<T>(tag: widget.tag)) {
+    if (widget.autoRemove && (_isCreator! || widget.assignId)) {
+      if (widget.global) {
         GetInstance().delete<T>(tag: widget.tag);
+      } else {
+        if(_controller is GetLifeCycleMixin) {
+          (_controller as GetLifeCycleMixin).onDelete();
+        }
       }
     }
 
