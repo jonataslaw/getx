f2614a8abd3c788e6062ca0f928815e93e57943a
shawon1fb <shawon0187@gmail.com>
2022-07-15 23:46:07 +0600

    fix type Otimized to Optimized lenght to length

--- lib/get_utils/src/widgets/optimized_listview.dart
+++ lib/get_utils/src/widgets/optimized_listview.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class OtimizedListView<T> extends StatelessWidget {
+class OptimizedListView<T> extends StatelessWidget {
   final List<T> list;
   final Axis scrollDirection;
   final bool reverse;
@@ -9,9 +9,10 @@ class OtimizedListView<T> extends StatelessWidget {
   final ScrollPhysics? physics;
   final bool shrinkWrap;
   final Widget onEmpty;
-  final int lenght;
+  final int length;
   final Widget Function(BuildContext context, ValueKey key, T item) builder;
-  const OtimizedListView({
+
+  const OptimizedListView({
     Key? key,
     required this.list,
     required this.builder,
@@ -22,8 +23,9 @@ class OtimizedListView<T> extends StatelessWidget {
     this.physics,
     this.onEmpty = const SizedBox.shrink(),
     this.shrinkWrap = false,
-  })  : lenght = list.length,
+  })  : length = list.length,
         super(key: key);
+
   @override
   Widget build(BuildContext context) {
     if (list.isEmpty) return onEmpty;
