acfcd61c71bc55500c08f7ed6b0ff180eed2a311
shawon1fb <shawon0187@gmail.com>
2022-07-16 00:13:54 +0600

    Subtract operator override to easily Subtract duration

--- lib/get_utils/src/extensions/date_time_extensions.dart
+++ lib/get_utils/src/extensions/date_time_extensions.dart
@@ -1,3 +1,7 @@
 extension GetDateTimeExtensions on DateTime {
+  /// Add Duration to DateTime
   DateTime operator +(Duration duration) => add(duration);
+
+  /// Subtracts Duration from DateTime
+  DateTime operator -(Duration duration) => subtract(duration);
 }
