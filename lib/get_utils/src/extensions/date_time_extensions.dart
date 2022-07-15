f4d624eee322af7efb0206a1717a9aeb3579752f
shawon1fb <shawon0187@gmail.com>
2022-07-16 00:19:39 +0600

    period of day

--- lib/get_utils/src/extensions/date_time_extensions.dart
+++ lib/get_utils/src/extensions/date_time_extensions.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/material.dart';
+
 extension GetDateTimeExtensions on DateTime {
   /// Add Duration to DateTime
   DateTime operator +(Duration duration) => add(duration);
 
   /// Subtracts Duration from DateTime
   DateTime operator -(Duration duration) => subtract(duration);
+
+  /// return time of day is before or after noon.
+  DayPeriod get period => TimeOfDay.fromDateTime(this).period;
+
+
 }
