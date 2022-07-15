6f469c3f311a22318b95efaeffd87df47c6e7ff1
shawon1fb <shawon0187@gmail.com>
2022-07-16 01:41:38 +0600

    timestamp test rename

--- test/utils/extensions/date_time_extension_test.dart
+++ test/utils/extensions/date_time_extension_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get_utils/src/extensions/date_time_extensions.dart';
 
 void main() {
-  test('var.isEqual(value)', () async {
+  test('timestamp test', () async {
     /// Date and time (GMT): Friday, July 15, 2022 6:24:54 PM
     var utcDate = DateTime.utc(2022, 7, 15, 18, 24, 54);
     expect(utcDate.unixTimeStamp == 1657909494, true);
