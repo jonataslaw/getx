1f70d71a48e369806f5b0501c736dad666c80125
shawon1fb <shawon0187@gmail.com>
2022-07-16 00:20:02 +0600

    datetime copywith

--- lib/get_utils/src/extensions/date_time_extensions.dart
+++ lib/get_utils/src/extensions/date_time_extensions.dart
@@ -10,5 +10,36 @@ extension GetDateTimeExtensions on DateTime {
   /// return time of day is before or after noon.
   DayPeriod get period => TimeOfDay.fromDateTime(this).period;
 
-
+  DateTime copyWith({
+    int? year,
+    int? month,
+    int? day,
+    int? hour,
+    int? minute,
+    int? second,
+    int? millisecond,
+    int? microsecond,
+  }) {
+    return isUtc
+        ? DateTime.utc(
+            year ?? this.year,
+            month ?? this.month,
+            day ?? this.day,
+            hour ?? this.hour,
+            minute ?? this.minute,
+            second ?? this.second,
+            millisecond ?? this.millisecond,
+            microsecond ?? this.microsecond,
+          )
+        : DateTime(
+            year ?? this.year,
+            month ?? this.month,
+            day ?? this.day,
+            hour ?? this.hour,
+            minute ?? this.minute,
+            second ?? this.second,
+            millisecond ?? this.millisecond,
+            microsecond ?? this.microsecond,
+          );
+  }
 }
