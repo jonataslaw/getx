69a15c3aac58eebc97b03569646e11a678d20c53
shawon1fb <shawon0187@gmail.com>
2022-07-16 22:08:50 +0600

    daysInMonth extension

--- lib/get_utils/src/extensions/date_time_extensions.dart
+++ lib/get_utils/src/extensions/date_time_extensions.dart
@@ -99,6 +99,24 @@ extension GetDateTimeExtensions on DateTime {
     };
     return _dayNameMap[weekday]!;
   }
+
+  int get daysInMonth {
+    final monthDays = <int, int>{
+      1: 31, // January
+      2: isLeapYear ? 29 : 28, // February
+      3: 31, // March
+      4: 30, // April
+      5: 31, // May
+      6: 30, // June
+      7: 31, // July
+      8: 31, // August
+      9: 30, // September
+      10: 31, // October
+      11: 30, // November
+      12: 31, // December
+    };
+    return monthDays[month]!;
+  }
 }
 
 ///[GetTimestamp] borrowed from firestore cloud store
--- test/utils/extensions/date_time_extension_test.dart
+++ test/utils/extensions/date_time_extension_test.dart
@@ -47,4 +47,45 @@ void main() {
     now = now + 1.days;
     expect(now.weekDayName == 'Fri', true);
   });
+
+  test('daysInMonth test', () {
+    var jan = DateTime(2022, 01, 16);
+    expect(jan.daysInMonth == 31, true);
+
+    var feb = DateTime(2022, 02, 16);
+    expect(feb.daysInMonth == 28, true);
+
+    var leapYearFeb = DateTime(2020, 02, 16);
+    expect(leapYearFeb.daysInMonth == 29, true);
+
+    var mar = DateTime(2022, 03, 16);
+    expect(mar.daysInMonth == 31, true);
+
+    var apr = DateTime(2022, 04, 16);
+    expect(apr.daysInMonth == 30, true);
+
+    var may = DateTime(2022, 05, 16);
+    expect(may.daysInMonth == 31, true);
+
+    var jun = DateTime(2022, 06, 16);
+    expect(jun.daysInMonth == 30, true);
+
+    var jul = DateTime(2022, 07, 16);
+    expect(jul.daysInMonth == 31, true);
+
+    var aug = DateTime(2022, 08, 16);
+    expect(aug.daysInMonth == 31, true);
+
+    var sep = DateTime(2022, 09, 16);
+    expect(sep.daysInMonth == 30, true);
+
+    var oct = DateTime(2022, 10, 16);
+    expect(oct.daysInMonth == 31, true);
+
+    var nov = DateTime(2022, 11, 16);
+    expect(nov.daysInMonth == 30, true);
+
+    var dec = DateTime(2022, 12, 16);
+    expect(dec.daysInMonth == 31, true);
+  });
 }
