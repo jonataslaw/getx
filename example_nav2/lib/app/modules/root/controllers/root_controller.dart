02d1e1a29d1c3a87aea86bcbdaedf1ba5c5dc6ea
Ricardo Dalarme <ricardoweasley@hotmail.com>
2022-08-13 17:55:47 -0300

    feat(lint): add flutter_lints to example_nav2

--- example_nav2/lib/app/modules/root/controllers/root_controller.dart
+++ example_nav2/lib/app/modules/root/controllers/root_controller.dart
@@ -4,15 +4,6 @@ class RootController extends GetxController {
   //TODO: Implement RootController
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
 
   @override
   void onClose() {}
--- example_nav2/lib/app/modules/settings/controllers/settings_controller.dart
+++ example_nav2/lib/app/modules/settings/controllers/settings_controller.dart
@@ -4,15 +4,6 @@ class SettingsController extends GetxController {
   //TODO: Implement SettingsController
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
 
   @override
   void onClose() {}
