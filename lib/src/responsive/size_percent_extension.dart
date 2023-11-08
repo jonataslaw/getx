2888a772a9fee20648c5a70ca8387fb3f343b024
Mahmoud Nabhan <13227239+niypoo@users.noreply.github.com>
2023-11-08 13:47:52 +0200

    add scalable pixel and split screen width to size_percent_extension.dart

--- lib/src/responsive/size_percent_extension.dart
+++ lib/src/responsive/size_percent_extension.dart
@@ -6,4 +6,16 @@ extension PercentSized on double {
   double get hp => (Get.height * (this / 100));
   // width: 30.0.hp = 30%
   double get wp => (Get.width * (this / 100));
+  
+  // split width in landscape (2 half)
+  double get swp => this *  (isLandscape ? (Get.width * 0.5) : Get.width) / 100;
+
+  /// Calculates the sp (Scalable Pixel) depending on the device's screen size
+  double get sp => this * ((isLandscape ? Get.height : Get.width) / fontSizeDividedRation) / 100;
+
+  // define what a ratio that I will divided font
+  int get fontSizeDividedRation => Get.context!.isPhone ? 3 : 5;
+
+  // if landscape
+  bool get isLandscape => Get.context!.isLandscape;
 }
