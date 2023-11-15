76091ed7ea5dc0c6afc350cd3643417f5de4643c
cervantes007G <cervantes007mail@gmail.com>
2023-11-15 11:59:04 -0500

    docs: test 0.0.6

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cervantes007G.github.io/getx',
+  url: 'https://cervantes007G.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/getx/',
