4ef01056e3ef5f597ff25ab2b86ea4751df19e19
keremkabiloglu <keremkabiloglu@kolayfirsat.com>
2024-05-06 11:43:35 +0300

    refactor: Update limitedSwipe default value in GetTransitionMixin

--- lib/get_navigation/src/routes/get_transition_mixin.dart
+++ lib/get_navigation/src/routes/get_transition_mixin.dart
@@ -607,7 +607,7 @@ Cannot read the previousTitle for a route that has not yet been installed''',
                   assert(_isPopGestureEnabled(route, canSwipe(route), context));
                   return _startPopGesture(route);
                 },
-                limitedSwipe: true,
+                limitedSwipe: limitedSwipe,
                 gestureWidth:
                     route.gestureWidth?.call(context) ?? _kBackGestureWidth,
                 initialOffset: initialOffset,
