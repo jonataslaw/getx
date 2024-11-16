278444324c5fa6e3f8c01f3d24b68844d1d63a68
mubashardev <microprogramer@yahoo.com>
2024-11-16 20:05:28 +0500

    fixed structure and moved onPopPage to onDidRemovePage

--- lib/get_navigation/src/routes/get_navigator.dart
+++ lib/get_navigation/src/routes/get_navigator.dart
@@ -5,8 +5,9 @@ import '../../../get.dart';
 class GetNavigator extends Navigator {
   GetNavigator.onGenerateRoute({
     GlobalKey<NavigatorState>? super.key,
-    bool Function(Route<dynamic>, dynamic)? onPopPage,
+    // bool Function(Route<dynamic>, dynamic)? onPopPage,
     required List<GetPage> super.pages,
+    super.onDidRemovePage,
     List<NavigatorObserver>? observers,
     super.reportsRouteUpdateToEngine,
     TransitionDelegate? transitionDelegate,
@@ -14,14 +15,14 @@ class GetNavigator extends Navigator {
     super.restorationScopeId,
   }) : super(
           //keys should be optional
-          onPopPage: onPopPage ??
-              (route, result) {
-                final didPop = route.didPop(result);
-                if (!didPop) {
-                  return false;
-                }
-                return true;
-              },
+          // onPopPage: onPopPage ??
+          //     (route, result) {
+          //       final didPop = route.didPop(result);
+          //       if (!didPop) {
+          //         return false;
+          //       }
+          //       return true;
+          //     },
           onGenerateRoute: (settings) {
             final selectedPageList =
                 pages.where((element) => element.name == settings.name);
