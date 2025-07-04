132f776296ad853574531ea2e24f17c431a99ec4
Husain Fadhilah <husainazkas@gmail.com>
2025-07-04 11:18:53 +0800

    chore: remove duplicate `firstWhereOrNull` util

--- lib/get_utils/src/extensions/iterable_extensions.dart
+++ lib/get_utils/src/extensions/iterable_extensions.dart
@@ -1,9 +1,18 @@
 extension IterableExtensions<T> on Iterable<T> {
   Iterable<TRes> mapMany<TRes>(
-      Iterable<TRes>? Function(T item) selector) sync* {
+    Iterable<TRes>? Function(T item) selector,
+  ) sync* {
     for (var item in this) {
       final res = selector(item);
       if (res != null) yield* res;
     }
+  }
+
+  /// The first element satisfying [test], or `null` if there are none.
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
   }
 }
